8e54f6b87b525d7e2cb99a0b117d95f292da32ea
thu-sama <tunlinthu94@gmail.com>
2018-10-05 11:50:51 +0900

    make files as npm package

---
+++ dist/src/index.js
@@ -0,0 +1,2 @@
+Object.defineProperty(exports,"__esModule",{value:true});var _Image=require("./Image");Object.defineProperty(exports,"Image",{enumerable:true,get:function get(){return _interopRequireDefault(_Image).default;}});var _CacheManager=require("./CacheManager");Object.defineProperty(exports,"CacheManager",{enumerable:true,get:function get(){return _interopRequireDefault(_CacheManager).default;}});function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
+//# sourceMappingURL=index.js.map
